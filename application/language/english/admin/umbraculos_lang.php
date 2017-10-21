9e8a5b9628ef77eed4326ad460e47ad92279babe
Andrés <darionenen@yahoo.com>
2017-10-21 19:01:12 -0300

    21 10

--- application/language/english/admin/umbraculos_lang.php
+++ application/language/english/admin/umbraculos_lang.php
@@ -10,22 +10,5 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['umbraculos_action']              = 'Acción';
-$lang['umbraculos_active']              = 'Activo';
-$lang['umbraculos_company']             = 'Company';
+
 $lang['umbraculos_create_umbraculo']         = 'Crear nuevo umbráculo';
-$lang['umbraculos_created_on']          = 'Creado el';
-$lang['umbraculos_deactivate_question'] = '¿Está seguro de querer desactivar la cuenta de %s';
-$lang['umbraculos_edit_user']           = 'Editar usuario';
-$lang['umbraculos_firstname']           = 'Nombre';
-$lang['umbraculos_groups']              = 'Tipo de usuario';
-$lang['umbraculos_inactive']            = 'Inactivo';
-$lang['umbraculos_ip_address']          = 'Dirección IP';
-$lang['umbraculos_last_login']          = 'Último acceso';
-$lang['umbraculos_lastname']            = 'Apellido';
-$lang['umbraculos_member_of_groups']    = 'Tipo de usuario';
-$lang['umbraculos_password']            = 'Contraseña';
-$lang['umbraculos_password_confirm']    = 'Confirmar contraseña';
-$lang['umbraculos_phone']               = 'Teléfono';
-$lang['umbraculos_status']              = 'Estado';
-$lang['umbraculos_username']            = 'Apodo';
