6f9166182d36b980098529fc9908f36c99bd20f0
Unknown <guly_09_@hotmail.com>
2017-10-23 15:19:30 -0300

    Mod insumo funcional

--- application/views/admin/insumos/edit.php
+++ application/views/admin/insumos/edit.php
@@ -33,38 +33,39 @@ document.getElementById("cantidad").value= contador--;
             <div class="col-md-12">
                  <div class="box">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Nuevo insumo</h3>
+                        <h3 class="box-title"><?php echo lang('insumos_edit_insumos');?></h3>
                     </div>
                     <div class="box-body">
+                      <!-- COMIENZA FORMULARIO -->
+                      <?php
+                      $attributes = array('name' => 'formulario');
+                      echo form_open('admin/insumos/edit/'.$insumo['idInsumo'], $attributes); ?>
 
-
-
-    <?php
-      $attributes = array('name' => 'formulario');
-      echo form_open('admin/insumos/edit/'.$insumo['idInsumo'], $attributes); ?>
-
-    	<div>
-    		<span class="text-danger">*</span>NombreInsumo :
-    		<input type="text" name="nombreInsumo" value="<?php echo ($this->input->post('nombreInsumo') ? $this->input->post('nombreInsumo') : $insumo['nombreInsumo']); ?>" />
+    	<div class="form-group">
+    		<label class='col-sm-2 control-label' >*<?php echo lang('insumos_name');?>:</label>
+        <div class="col-sm-10">
+          <input class="col-sm-10" type="text" name="nombreInsumo" value="<?php echo ($this->input->post('nombreInsumo') ? $this->input->post('nombreInsumo') : $insumo['nombreInsumo']); ?>" />
+        </div>
     		<span class="text-danger"><?php echo form_error('nombreInsumo');?></span>
     	</div>
-    	<div>
-        *Stock:
-       <input type="button" onClick="incrementar()" value="+">
-        <!-- ($this->input->post('cantidad') ? $this->input->post('cantidad') : $insumo['cantidad']);
-       echo  $insumo['cantidad']; -->
-    		<input  readonly="readonly" type="number" id="cantidad" name="cantidad"  value = "<?php echo  $insumo['cantidad']; ?>" />
-         <input type="button" onClick="decrementar()" value="-">
+
+      <div class="form-group">
+        <div class="col-sm-10">
+        <label class='col-sm-1 control-label' >*<?php echo lang('insumos_stock');?>:</label>
+        <input class="col-sm-1" type="button" onClick="incrementar()" value="+">
+    		<input  class="col-sm-1" readonly="readonly" type="number" id="cantidad" name="cantidad"  value = "<?php echo  $insumo['cantidad']; ?>" />
+         <input class="col-sm-1" type="button" onClick="decrementar()" value="-">
+        </div>
       </div>
     		<span class="text-danger"><?php echo form_error('cantidad');?></span>
 
-
-    	<div>
+    	<div class="form-group">
     		<?php echo lang('insumos_point');?>:
     		<input type="text" name="puntoDePedido" value="<?php echo ($this->input->post('puntoDePedido') ? $this->input->post('puntoDePedido') : $insumo['puntoDePedido']); ?>" />
     		<span class="text-danger"><?php echo form_error('puntoDePedido');?></span>
     	</div>
-    	<div>
+
+    	<div class="form-group">
     		<span class="text-danger">*</span> <?php echo lang('insumos_description');?>:
     		<textarea name="descripcionInsumo"><?php echo ($this->input->post('descripcionInsumo') ? $this->input->post('descripcionInsumo') : $insumo['descripcionInsumo']); ?></textarea>
     		<span class="text-danger"><?php echo form_error('descripcionInsumo');?></span>
