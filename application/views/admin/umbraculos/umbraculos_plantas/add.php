94dd19b59c86a57836f2a3dc5741aa4e5326f640
Andrés <darionenen@yahoo.com>
2017-10-26 11:32:52 -0300

    Probando modal
    Agregue un modal para seleccionar los datos de una planta y hacer las comparaciones

--- application/views/admin/umbraculos/umbraculos_plantas/add.php
+++ application/views/admin/umbraculos/umbraculos_plantas/add.php
@@ -23,19 +23,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<div class="col-md-6">
 						<label for="idPlanta" class="control-label"><span class="text-danger">*</span>Planta</label>
 						<div class="form-group">
-							<select name="idPlanta" class="form-control">
+                          <button type="button" class="btn btn-block btn-primary btn-flat'" data-toggle="modal" data-target="#myModal">Seleccionar planta</button>
+<!-- 							<select name="idPlanta" class="form-control">
 								<option value="">Seleccionar planta</option>
 								<?php 
 								foreach($all_plantas as $planta)
 								{
 									$selected = ($planta['idPlanta'] == $this->input->post('idPlanta')) ? ' selected="selected"' : "";
 									echo '<option value="'.$planta['idPlanta'].'" '.$selected.'>'.$planta['nombrePlanta'].'</option>';
-                                    echo "<input type='' name='' value='".($planta['idPlanta'] == $this->input->post('idPlanta'))."'>";
 								} 
+
 								?>
 							</select>
 							<span class="text-danger"><?php echo form_error('idPlanta');?></span>
-							<?php echo anchor('common/plantas/crear', '<i class="fa fa-plus"></i> ', array('class' => 'btn btn-block btn-primary btn-flat','title' => 'Registrar nueva planta')); ?>
+							<?php echo anchor('common/plantas/crear', '<i class="fa fa-plus"></i> ', array('class' => 'btn btn-block btn-primary btn-flat','title' => 'Registrar nueva planta')); ?> -->
 						</div>
 					</div>
 					<div class="col-md-6">
@@ -76,45 +77,71 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <?php echo form_close(); ?>
                          </div>
                     </div>
+<!-- probando modal-->
+<div class="container">
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title"><span class="fa fa-tencent-weibo"></span> Seleccionar planta</h4>
+        </div>
+        <div class="modal-body">
+                          <table class="table table-striped table-hover">
+                    <tr>
+                    <div></div>
+                        <th>Nombre</th>
+                        <th>Especie</th>
+                        <th>Unidades Espacio</th>
+                        <th>Acciones</th>
+                    </tr>
+                    <?php foreach($all_plantas as $p){ ?>
+                    <tr>
+                        <td><?php echo $p['nombrePlanta']; ?></td> 
+                        <td><?php echo $p['nombreEspecie']; ?></td>
+                        <td><?php echo $p['unidadEspacioPlanta_m2']; ?>cm<sup>2</sup></td>
+                        <td>
+                            <button class="btn btn-info btn-xs"  data-dismiss="modal"> <span class="fa fa-check"></span> Seleccionar</button>
+<!--                             <a href="<?php echo site_url('common/plantas/editar/'.$p['idPlanta']); ?>" class="btn btn-info btn-xs"><span class="fa fa-pencil"></span> Editar</a> 
+                            <a href="<?php echo site_url('plantas/remove/'.$p['idPlanta']); ?>" class="btn btn-danger btn-xs"><span class="fa fa-trash"></span> Borrar</a> -->
+                        </td>
+                    </tr>
+                    <?php } ?>
+                </table>
+                <?php echo anchor('common/plantas/crear', '<i class="fa fa-plus"></i> ', array('class' => 'btn btn-block btn-primary btn-flat','title' => 'Registrar nueva planta')); ?>
+        </div>
+        <div class="modal-footer">
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+</div>
                 </section>
             </div>
 
+
+  <style>
+  .modal-header, h4, .close {
+      background-color: #5cb85c;
+      color:white !important;
+      text-align: center;
+      font-size: 30px;
+  }
+  .modal-footer {
+      background-color: #f9f9f9;
+  }
+  </style>
+
+
 <script>
-    function aplicarFiltro() {
-
-var req = $.ajax({
-    url: BASE_URL+"cli/Autorizaciones/set_filtro_autorizaciones",
-    type: "POST",
-    data:
-    {
-        'conductor': $("#f-conductor").val(),
-        'patente': $("#f-patente").val(),
-        'pasajeros': $("#f-pasajeros").val(),
-        'puntaje': $("#f-puntaje").val(),
-        'autoriza': $("#f-autoriza").val(),
-        'origen': $("#f-origen").val(),
-        'destino': $("#f-destino").val(),
-        'salida': $("#f-salida").val(),
-        'arribo': $("#f-arribo").val(),
-        'telefono': $("#f-telefono").val(),
-        'imei': $("#f-imei").val(),
-        'enviado': $("#f-enviado").val(),
-        'recibido': $("#f-recibido").val(),
-        'latitud': $("#f-latitud").val(),
-        'longitud': $("#f-longitud").val()
-      },
-    dataType: "json",
-    ContentType: 'application/json; charset=utf-8',
-    traditional: true,
-    error: function(ts) { console.log(ts); },
-    success: refreshGrid
-
+$(document.ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+    });
 });
-}
-
-function refreshGrid(resultado)
-{
-  console.log(resultado); //ASÍ PODES VER EN LA CONSOLA CÓMO ESTA LLEGANDO EL OBJETO
-  //MANIPULO resultado PARA METER LA INFO EN LA PÁGINA
-}
 </script>
