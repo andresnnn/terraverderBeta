1019c4b2f904235d61bf348e826235fa50e1932f
Unknown <guly_09_@hotmail.com>
2017-10-30 00:44:39 -0300

    dir arreglada

--- application/views/admin/umbraculos/umbraculo_tarea/atender.php
+++ application/views/admin/umbraculos/umbraculo_tarea/atender.php
@@ -3,35 +3,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 ?>
 
-            <div class="content-wrapper">
-                <section class="content-header">
-                    <?php echo $pagetitle; ?>
-                    <?php echo $breadcrumb; ?>
+<div class="content-wrapper">
+    <section class="content-header">
+        <?php echo $pagetitle; ?>
+        <?php echo $breadcrumb; ?>
+    </section>
 
-                </section>
-
-                <section class="content">
-                    <div class="row">
-                        <div class="col-md-12">
-                             <div class="box">
-                                <div class="box-body">
-			<div class="box-header with-border">
-             <h3 class="box-title">Atender tarea de <strong>></strong></h3>
+    <section class="content">
+      <div class="row">
+    <div class="col-md-12">
+      	<div class="box box-info">
+            <div class="box-header with-border">
+              	<h3 class="box-title">Tarea Edit</h3>
             </div>
-            <?php echo form_open('atender/'.$id); ?>
-          	<div class="box-body">
+			<?php echo form_open('tareas/edit/'.$tarea['idTarea']); ?>
+			<div class="box-body">
+				<div class="row clearfix">
+					<div class="col-md-6">
+						<label for="idTipoTarea" class="control-label">Tipotarea</label>
+						<div class="form-group">
+							<select name="idTipoTarea" class="form-control">
+								<option value="">select tipotarea</option>
+								<?php
+								foreach($all_tipotarea as $tipotarea)
+								{
+									$selected = ($tipotarea['idTipoTarea'] == $tarea['idTipoTarea']) ? ' selected="selected"' : "";
 
-              <label for="fechaComienzo" class="col-md-3 control-label">Fecha Prevista</label>
-          		<div class="col-md-8">
-          			<input readonly type="text" name="fechaComienzo" value="<?php echo ($this->input->post('fechaComienzo') ? $this->input->post('fechaComienzo') : $tarea['fechaComienzo']); ?>" class="form-control" id="fechaComienzo" />
-          		</div>
-
-              <div class="col-md-6">
-
+									echo '<option value="'.$tipotarea['idTipoTarea'].'" '.$selected.'>'.$tipotarea['nombreTipoTarea'].'</option>';
+								}
+								?>
+							</select>
+						</div>
+					</div>
+					<div class="col-md-6">
 						<label for="idEstado" class="control-label">Estado Tarea</label>
 						<div class="form-group">
 							<select name="idEstado" class="form-control">
-								<option value="">select estado tarea</option>
+								<option value="">select estado_tarea</option>
 								<?php
 								foreach($all_estado_tarea as $estado_tarea)
 								{
@@ -43,20 +51,96 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							</select>
 						</div>
 					</div>
-
-
 					<div class="col-md-6">
-						<label for="idTipoTarea" class="control-label"><span class="text-danger">*</span>Tipotarea</label>
+						<label for="idUserAtencion" class="control-label">User</label>
+						<div class="form-group">
+							<select name="idUserAtencion" class="form-control">
+								<option value="">select user</option>
+								<?php
+								foreach($all_users as $user)
+								{
+									$selected = ($user['id'] == $tarea['idUserAtencion']) ? ' selected="selected"' : "";
+
+									echo '<option value="'.$user['id'].'" '.$selected.'>'.$user['id'].'</option>';
+								}
+								?>
+							</select>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="idUserCreador" class="control-label">User</label>
+						<div class="form-group">
+							<select name="idUserCreador" class="form-control">
+								<option value="">select user</option>
+								<?php
+								foreach($all_users as $user)
+								{
+									$selected = ($user['id'] == $tarea['idUserCreador']) ? ' selected="selected"' : "";
+
+									echo '<option value="'.$user['id'].'" '.$selected.'>'.$user['id'].'</option>';
+								}
+								?>
+							</select>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="idUmbraculo" class="control-label">Umbraculo</label>
+						<div class="form-group">
+							<select name="idUmbraculo" class="form-control">
+								<option value="">select umbraculo</option>
+								<?php
+								foreach($all_umbraculo as $umbraculo)
+								{
+									$selected = ($umbraculo['idUmbraculo'] == $tarea['idUmbraculo']) ? ' selected="selected"' : "";
+
+									echo '<option value="'.$umbraculo['idUmbraculo'].'" '.$selected.'>'.$umbraculo['idUmbraculo'].'</option>';
+								}
+								?>
+							</select>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="fechaCreacion" class="control-label">FechaCreacion</label>
+						<div class="form-group">
+							<input type="text" name="fechaCreacion" value="<?php echo ($this->input->post('fechaCreacion') ? $this->input->post('fechaCreacion') : $tarea['fechaCreacion']); ?>" class="has-datepicker form-control" id="fechaCreacion" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="fechaAtencion" class="control-label">FechaAtencion</label>
+						<div class="form-group">
+							<input class="has-datepicker form-control" type="date" id="fechaAtencion"  name="fechaAtencion" value="<?php echo ($this->input->post('fechaAtencion') ? $this->input->post('fechaAtencion') : $tarea['fechaAtencion']); ?>" class="has-datepicker form-control" id="fechaAtencion" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="fechaHoraComienzo" class="control-label">FechaComienzo</label>
+						<div class="form-group">
+							<input type="text" name="fechaComienzo" value="<?php echo ($this->input->post('fechaComienzo') ? $this->input->post('fechaComienzo') : $tarea['fechaComienzo']); ?>" class="has-datepicker form-control" id="fechaComienzo" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="idPlanta" class="control-label">IdPlanta</label>
+						<div class="form-group">
+							<input type="text" name="idPlanta" value="<?php echo ($this->input->post('idPlanta') ? $this->input->post('idPlanta') : $tarea['idPlanta']); ?>" class="form-control" id="idPlanta" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="observacionEspecialista" class="control-label">ObservacionEspecialista</label>
+						<div class="form-group">
+							<textarea name="observacionEspecialista" class="form-control" id="observacionEspecialista"><?php echo ($this->input->post('observacionEspecialista') ? $this->input->post('observacionEspecialista') : $tarea['observacionEspecialista']); ?></textarea>
+							<span class="text-danger"><?php echo form_error('observacionEspecialista');?></span>
+						</div>
+					</div>
+				</div>
+			</div>
+
+        <div class="box-footer" style="text-align: center;">
+            <button type="submit" class="btn btn-primary btn-flat">Agregar</button>
+            <a href="<?php echo site_url('common/umbraculos/ver/'.$id); ?>" class="btn btn-default btn-flat">Cancelar</a>
         </div>
-            <div class="box-footer" style="text-align: center;">
-                <button type="submit" class="btn btn-primary btn-flat">Agregar</button>
-                <a href="<?php echo site_url('common/umbraculos/ver/'.$id); ?>" class="btn btn-default btn-flat">Cancelar</a>
-            </div>
-            </div>
-            <?php echo form_close(); ?>
-                         </div>
-                    </div>
-                  </div>
-             </div>
-                </section>
-            </div>
+
+			<?php echo form_close(); ?>
+		</div>
+    </div>
+</div>
+</section>
+</div>
--- application/views/admin/umbraculos/umbraculo_tarea/see.php
+++ application/views/admin/umbraculos/umbraculo_tarea/see.php
@@ -37,10 +37,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                                 <!-- IF TAREA ESTADO = 1 MUESTRO ESTE BOTON -->
                                                 <?php if ($t['idEstado'] == 1) {
                                                 //echo " <a href= 'atender/'.$u['idTarea'];"
+                                                echo anchor('common/umbraculos/atender/'.$t['idTarea'], '<i class="fa fa-eye"></i> '.'Atender tarea', array('class' => 'btn btn-block btn-primary btn-flat','title'=>'(?) Atender tarea'));
+                                                    //echo "<a href= atender/"; echo $t['idTarea'];
 
-                                                    echo "<a href= atender/"; echo $t['idTarea'];
-
-                                                  echo" class='btn btn-danger btn-xs'><span class='fa fa-sign-language'></span> Atender</a>";
+                                                  // echo" class='btn btn-danger btn-xs'><span class='fa fa-sign-language'></span> Atender</a>";
                                                 } ?>
                                                 </td>
                                             </tr>
