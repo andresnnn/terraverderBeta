c3ab8666602df791f830992b43a184244721427b
Unknown <guly_09_@hotmail.com>
2017-11-02 00:10:37 -0300

    no andaa

--- application/views/admin/umbraculos/umbraculo_tarea/atender.php
+++ application/views/admin/umbraculos/umbraculo_tarea/atender.php
@@ -24,11 +24,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                   <th>Planta</th>
                                   <th>Umbraculo</th>
                                   <th>Creador</th>
-
                                   <th>Fecha Creación</th>
                                   <th>Fecha Prevista</th>
                                   <th>Estado Actual </th>
-
                               </tr>
                               <?php foreach($tarea as $t){ ?>
                               <tr>
@@ -48,18 +46,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 </div>
               </div>
 
-
   <div class="row">
     <div class="col-md-12">
       	<div class="box box-info">
             <div class="box-header with-border">
-              	<h3 class="box-title">Tarea Edit</h3>
+              	<h3 class="box-title">Atender Tarea</h3>
             </div>
 			<?php echo form_open('common/umbraculos/atenderTarea/'.$idUmbraculo.'/'.$idTarea); ?>
 			<div class="box-body">
 				<div class="row clearfix">
 
-					<div class="col-md-6">
+					<div class="col-md-3">
 						<label for="idEstado" class="control-label">Estado Tarea</label>
 						<div class="form-group">
 							<select name="idEstado" class="form-control">
@@ -74,7 +71,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								?>
 							</select>
 						</div>
-
 					</div>
 
 
@@ -95,6 +91,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<span class="text-danger"><?php echo form_error('fechaAtencion');?></span>
 						</div>
           </div>
+
 <?php foreach($tarea as $t){ ?>
       <div class="col-md-6">
             <label for="observacionEspecialista" class="control-label">ObservacionEspecialista</label>
@@ -103,75 +100,44 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             </div>
           </div>
 <?php } ?>
-
-            <!-- seleccionar insumo -->
-            <div class="col-md-6">
-              <label for="idInsumo" class="control-label"><span class="text-danger">*</span>Insumo</label>
-              <div class="form-group">
-                                  <input disabled class="form-control" type="text" name="nombre" id="nombre" value=""/>
-                                  <button type="button" class="btn btn-block btn-primary btn-flat'" data-toggle="modal" data-target="#myModal"> <span class="fa fa-plus"></span>Seleccionar Insumo</button>
-                                  <span class="text-danger"><?php echo form_error('idInsumo');?></span> <!-- ESTE SERIA EL CAMPO DONDE INFORMARIA EL ERROR-->
-                                  <span id="estadoT" class="text-danger"></span><br>
-                                  <!-- <span id="estadoL" class="text-danger"></span><br> -->
-                                  <!-- <span id="estadoH" class="text-danger"></span><br> -->
-                                  <input type="hidden" min="0" name="idPlanta" value="<?php echo $this->input->post('idInsumo'); ?>" class="form-control" id="idInsumo" />
-              </div>
-                                    <!--CAMPOS QUE VAN A SER ENVIADOS AL CONTROLADOR PARA CARGARSE EN LA 'BD'-->
-                      <input type="text" name="idInsumo" id="idInsumo">
-                      <input type="text" name="cantidad" id="cantidad">
-                      <input type="hidden" name="nuevo_stock" id="nuevo_stock">
-                      <!--FIN DE CAMPOS OCULTOS-->
-            </div>
-
         </div>
 			</div>
+            <!-- seleccionar insumo -->
 
-      <!-- probando modal-->
-      <div class="container">
-        <!-- Modal -->
-        <div class="modal fade" id="myModal" role="dialog">
-          <div style="overflow-x:auto;" class="modal-dialog modal-lg">
 
-            <!-- Modal content-->
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title"><span class="fa fa-tencent-weibo"></span> Seleccionar Insumos</h4>
-              </div>
-              <div class="modal-body">
-                      <table class="table table-striped table-hover">
-                          <tr>
-                          <div></div>
-                              <th>Nombre</th>
-                              <th>Stock</th>
-                              <th>Cantidad Requerida</th>
-                              <th>Acciones</th>
-                          </tr>
-                          <?php foreach($insumos as $p){ ?>
-                              <?php if ($p['active'] == 1): ?>
-                                  <tr id="<?php echo 'insumo'.$p['idInsumo'];?>">
-                                      <td id="nombreInsumo"><?php echo $p['nombreInsumo']; ?></td>
-                                      <td><?php echo $p['cantidad']; ?></td>
-                                      <td> <input  id="cantidadUtilizada" type="number" min="0" max="<?php echo $p['cantidad']; ?>" name="cantidadUtilizada"  /></td>
-                                      <td><input type="hidden" name="identificador" value="<?php echo $p['idInsumo'];?>"></td>
-                                      <td class="boton">
-                                          <button onClick="javascript:llenarFormulario(<?php echo $p['idInsumo'];?>);" class="btn btn-info btn-xs"  data-dismiss="modal"> <span class="fa fa-check"></span> Utilizar</button>
-                                      </td>
-                                  </tr>
-                              <?php endif; ?>
-                          <?php } ?>
-                      </table>
-                      <?php echo anchor('common/insumos/add', '<i class="fa fa-plus"> Agregar nuevo Insumo </i> ', array('class' => 'btn btn-block btn-primary btn-flat','title' => 'Registrar nueva planta')); ?>
-              </div>
-              <div class="modal-footer">
-              </div>
-            </div>
 
-          </div>
-        </div>
+<!-- script tabla dinamica -->
 
-      </div>
+<script>
+    $(document).ready(function(){
+        var i = 1;
+        $('#add').click(function(){
+            i++;
+            $('#dynamic_field').append('<tr id="row'+i+'"><td><div class="field-wrap"><label>Company<span class="req">*</span></label><input type="text" required autocomplete="off" name="company[]"/></div><div class="field-wrap"><label>Project<span class="req">*</span></label><input type="text"required autocomplete="off" name="project[]"/></div><div class="field-wrap"><label>Duration<span class="req">*</span></label><input type="text"required autocomplete="off" name="duration[]"/></div><div class="field-wrap"><label>Key Learning<span class="req">*</span></label><input type="text"required autocomplete="off" name="key_learning[]"/></div></td></td><td><button name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');
+        });
 
+        $(document).on('click','.btn_remove', function(){
+            var button_id = $(this).attr("id");
+            $("#row"+button_id+"").remove();
+        });
+
+        $('#submit').click(function(){
+            $.ajax({
+                async: true,
+                url: "internship_details.php",
+                method: "POST",
+                data: $('#internship_details').serialize(),
+                success:function(rt)
+                {
+                    alert(rt);
+                    $('#internship_details')[0].reset();
+                }
+            });
+        });
+    });
+</script>
+
+      <!-- pie de paginal-->
       <div class="box-footer">
           <button type="submit" class="btn btn-primary btn-flat">Guardar</button>
           <a href="<?php echo site_url('common/umbraculos/verTareas/'.$idUmbraculo); ?>" class="btn btn-default btn-flat">Cancelar</a>
@@ -185,40 +151,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </section>
 </div>
 
-<!-- Estilo del modal -->
-<style>
-.modal-header, h4, .close {
-    background-color: #5cb85c;
-    color:white !important;
-    text-align: center;
-    font-size: 30px;
-}
-.modal-footer {
-    background-color: #f9f9f9;
-}
-</style>
-
-
-
-<!-- script modal -->
-<script>
-//FUNCION PARA ABRIR LA VENTANA MODAL
-//
-    $(document.ready(function(){
-        $("#myBtn").click(function(){
-            $("#myModal").modal();
-        });
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+    var maxField = 10; //Input fields increment limitation
+    var addButton = $('.add_button'); //Add button selector
+    var wrapper = $('.field_wrapper'); //Input field wrapper
+    var fieldHTML = '<div><input type="text" name="field_name[]" value=""/><a href="javascript:void(0);" class="remove_button" title="Remove field"><img src="remove-icon.png"/></a></div>'; //New input field html
+    var x = 1; //Initial field counter is 1
+    $(addButton).click(function(){ //Once add button is clicked
+        if(x < maxField){ //Check maximum number of input fields
+            x++; //Increment field counter
+            $(wrapper).append(fieldHTML); // Add field html
+        }
     });
-
-/**
- * [cargarDatos description]
- * @param  {[type]} id ES EL ID DE LA PLANTA DENTRO DE LA BD
- * @return {[type]}    [description]
- * @author SAKZEDMK
- */
-function llenarFormulario(id) {
-    document.getElementById('cantidad').value = document.getElementById('fila'+id).cells[2].innerHTML;
-    document.getElementById('identificador').value = document.getElementById('fila'+id).cells[3].innerHTML;
-}
-
+    $(wrapper).on('click', '.remove_button', function(e){ //Once remove button is clicked
+        e.preventDefault();
+        $(this).parent('div').remove(); //Remove field html
+        x--; //Decrement field counter
+    });
+});
 </script>
