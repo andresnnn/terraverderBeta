23dccb3417d1bd3af4d18c41a4d6efa83c880b28
Pablo Martín Alanis <pabloalanis_1@hotmail.com>
2017-11-03 21:23:38 -0300

    crear umbraculo corregido
    cambie el disabled por readOnly...ya que pedia el campo la consulta

--- application/views/admin/umbraculos/crear.php
+++ application/views/admin/umbraculos/crear.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 ?>
 
+
             <div class="content-wrapper">
                 <section class="content-header">
                     <?php echo $pagetitle; ?>
@@ -14,6 +15,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <div class="row">
                         <div class="col-md-12">
                              <div class="box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Crear nuevo Umbráculo</h3>
+                    </div>
                                 <div class="box-body">
                                     <?php echo form_open('common/umbraculos/crear'); ?>
                                                 <div class="box-body">
@@ -28,28 +32,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                                         <div class="col-md-6">
                                                             <label for="anchoUmbraculo_m" class="control-label"><span class="text-danger"></span>Ancho (m<sup>2</sup>)</label>
                                                             <div class="form-group">
-                                                                <input type="number" name="anchoUmbraculo_m" value="<?php echo $this->input->post('anchoUmbraculo_m'); ?>" class="form-control" id="anchoUmbraculo_m" />
+                                                                <input type="number" step="any" name="anchoUmbraculo_m" value="<?php echo $this->input->post('anchoUmbraculo_m'); ?>" class="form-control" id="anchoUmbraculo_m" />
                                                                 <span class="text-danger"><?php echo form_error('anchoUmbraculo_m');?></span>
                                                             </div>
                                                         </div>
                                                         <div class="col-md-6">
                                                             <label for="largoUmbraculo_m" class="control-label"><span class="text-danger"></span>Largo (m<sup>2</sup>)</label>
                                                             <div class="form-group">
-                                                                <input type="number" name="largoUmbraculo_m" value="<?php echo $this->input->post('largoUmbraculo_m'); ?>" class="form-control" id="largoUmbraculo_m" />
+                                                                <input type="number" step="any" name="largoUmbraculo_m" onChange="completar();" value="<?php echo $this->input->post('largoUmbraculo_m'); ?>" class="form-control" id="largoUmbraculo_m" />
                                                                 <span class="text-danger"><?php echo form_error('largoUmbraculo_m');?></span>
                                                             </div>
                                                         </div>
                                                         <div class="col-md-6">
                                                             <label for="unidadEspacioTotal_m2" class="control-label"><span class="text-danger"></span>Espacio Total (m<sup>2</sup>)</label>
                                                             <div class="form-group">
-                                                                <input type="number" name="unidadEspacioTotal_m2" value="<?php echo $this->input->post('unidadEspacioTotal_m2'); ?>" class="form-control" id="unidadEspacioTotal_m2" />
+                                                                <input  type="number" step="any" name="unidadEspacioTotal_m2" value="<?php echo $this->input->post('unidadEspacioTotal_m2'); ?>" class="form-control" id="unidadEspacioTotal_m2" readonly/>
                                                                 <span class="text-danger"><?php echo form_error('unidadEspacioTotal_m2');?></span>
                                                             </div>
                                                         </div>
                                                         <div class="col-md-6">
                                                             <label for="unidadEspacioDisponible_m2" class="control-label">Espacio Disponible (m<sup>2</sup>)</label>
                                                             <div class="form-group">
-                                                                <input type="number" name="unidadEspacioDisponible_m2" value="<?php echo $this->input->post('unidadEspacioDisponible_m2'); ?>" class="form-control" id="unidadEspacioDisponible_m2" />
+                                                                <input  type="number" step="any" name="unidadEspacioDisponible_m2" value="<?php echo $this->input->post('unidadEspacioDisponible_m2'); ?>" class="form-control" id="unidadEspacioDisponible_m2" readonly/><!--disabled-->
                                                             </div>
                                                         </div>
                                                         <div class="col-md-6">
@@ -92,10 +96,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                                     <div class="box-footer">
                                                         <button type="submit" class="btn btn-primary btn-flat">Guardar</button>
                                                         <a href="<?php echo site_url('common/umbraculos'); ?>" class="btn btn-default btn-flat">Cancelar</a>
-                                                    </div>  
+                                                    </div>
                                                 <?php echo form_close(); ?>
                          </div>
                     </div>
                 </section>
             </div>
+<script>
 
+    function completar()
+    {
+        var ancho = document.getElementById('anchoUmbraculo_m').value;
+        var largo = document.getElementById('largoUmbraculo_m').value;
+        var tot = ancho * largo;
+        document.getElementById('unidadEspacioTotal_m2').value= tot.toFixed(3);
+        document.getElementById('unidadEspacioDisponible_m2').value=tot.toFixed(3);
+    }
+</script>
