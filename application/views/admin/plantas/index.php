9bd64f4634af0faf8b5964c895fcf1548cd7ecc8
Unknown <guly_09_@hotmail.com>
2018-03-24 23:47:54 -0300

    editar planta
    el editar planta esta desactivado si la planta esta en umbraculo

--- application/views/admin/plantas/index.php
+++ application/views/admin/plantas/index.php
@@ -45,7 +45,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         <th>Nombre Científico</th>
                         <th>Unidades Espacio</th>
                         <th>Descripción</th>
-                        <th>Estado</th>
+                        <th>Estado<a title="El estado define el activado o desactivado de plantas" href="#aboutModal" data-toggle="modal" data-target="#myModal" class="btn fa fa-info-circle fa-lg "></a></th>
                         <th>Acciones</th>
                     </tr>
                     <?php foreach($plantasCon as $p){ ?>
@@ -65,16 +65,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 } else if ($p['pa'] == 0 and $p['ea'] == 1){
                                     echo "<a href='".site_url('common/plantas/activado_logico/'.$p['idPlanta'])."'><span class='label label-default'>Inactivo</span></a>";
                                 }
-                                
+
                             ?>
                         </td>
                         <td>
                             <a href="<?php echo site_url('common/plantas/ver/'.$p['idPlanta']); ?>" class="btn btn-warning btn-xs"><span class="fa fa-eye"></span> Ver</a>
-                            <a href="<?php echo site_url('common/plantas/editar/'.$p['idPlanta']); ?>" class="btn btn-info btn-xs"><span class="fa fa-pencil"></span> Editar</a>
+                            <a style='pointer-events: none;' href="<?php echo site_url('common/plantas/editar/'.$p['idPlanta']); ?>" class="btn btn-info btn-xs"><span class="fa fa-pencil"></span> Editar</a>
                         </td>
                     </tr>
                     <?php } ?>
-                    
+
                     <?php foreach($plantasSin as $pp){ ?>
                     <tr>
                         <td><?php echo $pp['nombrePlanta']; ?></td>
@@ -92,7 +92,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 } else if ($pp['pa'] == 0 and $pp['ea'] == 1){
                                     echo "<a href='".site_url('common/plantas/activado_logico/'.$pp['idPlanta'])."'><span class='label label-default'>Inactivo</span></a>";
                                 }
-                                
+
                             ?>
                         </td>
                         <td>
@@ -101,7 +101,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         </td>
                     </tr>
                     <?php } ?>
-                    
+
                 </table>
                          </div>
                     </div>
